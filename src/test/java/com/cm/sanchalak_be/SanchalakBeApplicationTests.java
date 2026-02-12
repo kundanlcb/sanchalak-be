ba72d89a6e33a3f4014b9e0b84c4c0d280704335
kundan <kundanlcb@gmail.com>
2026-02-12 14:02:46 +0530

    Initial commit: Spring Boot scaffold

---
+++ src/test/java/com/cm/sanchalak_be/SanchalakBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.sanchalak_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanchalakBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
