a77c130d1944e2aa9cfdf2b6fccd4bcb71d69531
kundan <kundanlcb@gmail.com>
2026-03-02 08:59:46 +0530

    fix(academics): resolve parameter exception in exam question specification

--- src/main/java/com/cm/sanchalak/repository/spec/ExamQuestionSpecification.java
+++ src/main/java/com/cm/sanchalak/repository/spec/ExamQuestionSpecification.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class ExamQuestionSpecification extends BaseSpecification {
     public static Specification<ExamQuestion> activeScoped() {
-        return BaseSpecification.scoped("examSchedule.studentClass");
+        return BaseSpecification.scoped("examSchedule");
     }
 
     public static Specification<ExamQuestion> activeById(Long id) {
